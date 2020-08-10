5ddf04fed7e50e5776c1ece3d4620a91f3bb625c
kien121998 <kiennguyenvnmos@gmail.com>
2020-08-10 19:12:55 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import Camera from './components/Camera.js';
+import { StyleSheet, View } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props){
+      super(props);
+      process.nextTick = setImmediate;
+  }
+  render() {
+      return (
+          <View style={styles.container}>
+              <Camera />
+          </View>
+      );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',   
+  }
+});
